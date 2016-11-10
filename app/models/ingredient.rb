079492cd38b06b957b23402181eb74fe9d9e24da
Imre Mocsari <imremocsari1@yahoo.co.uk>
2016-11-10 16:19:36 +0000

    back end

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,5 +1,5 @@
 class Ingredient < ApplicationRecord
   validates :name, presence: true, uniqueness: true
-  has_many :doses, dependent: :destroy
+  has_many :doses, dependent: :restrict_with_exception
   has_many :cocktails, through: :doses
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :doses
+  root 'cocktails#index'
+  resources :cocktails, only: [:show, :index, :new, :create, :edit, :destroy] do
+    resources :doses, only: [:new, :create]
+  end
+  resources :doses, only: [:destroy]
   resources :ingredients
-  resources :cocktails
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'cocktail#index'
 end

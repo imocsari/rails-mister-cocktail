fae9d582b0d932411f37a93aca73cf130b9515b2
Imre Mocsari <imremocsari1@yahoo.co.uk>
2016-11-10 12:55:18 +0000

    after scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :doses
+  resources :ingredients
+  resources :cocktails
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'cocktail#index'
 end
